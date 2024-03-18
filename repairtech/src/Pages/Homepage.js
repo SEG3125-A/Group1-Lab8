743b67113a7a4376694e6b065be680ad3ad8d22b
Dante Deslauriers <64750637+ddesl069@users.noreply.github.com>
2024-03-17 22:27:44 -0400

    dd

--- repairtech/src/Pages/Homepage.js
+++ repairtech/src/Pages/Homepage.js
@@ -2,7 +2,20 @@
 import React from 'react';
 
 function HomePage() {
-  return <div>Home Page</div>;
+  return(
+    
+    
+      
+    <div className="home-page-container"> {/* Add this container */}
+    <div className="content-wrap">
+      {/* Your main content goes here. Add more content or elements as needed */}
+    </div>
+    <footer className="App-footer">
+      © 2024 RepairTech, Inc. All rights reserved.
+    </footer>
+  </div>
+    
+  );
 }
 
 export default HomePage;
