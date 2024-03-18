98d632f5fdf6a3d7b1b91320c93cfb0e25c917e5
Githuber-3000 <135640261+Githuber-3000@users.noreply.github.com>
2024-03-17 21:37:52 -0400

    brought back navbar button

--- repairtech/src/Navbar.js
+++ repairtech/src/Navbar.js
@@ -1,12 +1,24 @@
-// Navbar.js
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Navbar() {
+function Navbar({ closeNavbar }) {
   const location = useLocation();
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   const [pageTitle, setPageTitle] = useState('');
 
-  // Update the page title whenever the location changes
+  // Function to close the navbar when a link is clicked
+  const handleNavLinkClick = () => {
+    setIsNavExpanded(false);
+    if (closeNavbar) closeNavbar();
+  };
+
+  // Function to handle hamburger button click
+  const handleButtonClick = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
+  // Update page title when the location changes
   useEffect(() => {
     switch (location.pathname) {
       case '/Pages/Homepages.js':
@@ -30,19 +42,23 @@ function Navbar() {
 
   return (
     <>
+      <div className='menu-button'>
+        <button className="btn btn-primary hamburger-btn" onClick={handleButtonClick}>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+        </button>
+      </div>
+      <div className={`sidebar bg-light ${isNavExpanded ? 'expanded' : 'collapsed'}`} style={{ width: isNavExpanded ? '250px' : '0', transition: '0.3s' }}>
+        <a href="#" className="closebtn" onClick={() => setIsNavExpanded(false)}>&times;</a>
+        {/* Use handleNavLinkClick to close navbar on click */}
+        <Link to="/Pages/Homepages.js" onClick={handleNavLinkClick}>Homepage</Link>
+        <Link to="/Pages/Services.js" onClick={handleNavLinkClick}>Services</Link>
+        <Link to="/Pages/Technicians.js" onClick={handleNavLinkClick}>Technicians</Link>
+        <Link to="/Pages/AboutUs.js" onClick={handleNavLinkClick}>About Us</Link>
+      </div>
       <div className="title-band">
         <h1 className="page-title">{pageTitle}</h1>
-      </div>
-      <div className='menu-button'>
-          {/* Your hamburger button code here */}
-      </div>
-      <div className={`sidebar bg-light`}>
-        <a href="#" className="closebtn">&times;</a>
-        {/* Use handleNavLinkClick to close navbar on click */}
-        <Link to="/Pages/Homepages.js">Homepage</Link>
-        <Link to="/Pages/Services.js">Services</Link>
-        <Link to="/Pages/Technicians.js">Technicians</Link>
-        <Link to="/Pages/AboutUs.js">About Us</Link>
       </div>
     </>
   );
