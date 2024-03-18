0bc5101dbde14ba4f92081a6ce61412b2df38e22
Callum F <larrylion01@gmail.com>
2024-03-17 22:27:41 -0400

    pushing changes to package.json for calendar, and the onlinebooking as well as css

--- repairtech/src/Pages/Onlinebooking.jsx
+++ repairtech/src/Pages/Onlinebooking.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 function OnlineBooking() {
-  return <div>Online Booking</div>;
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  return (
+  
+  
+  <div className='online-booking-container'>
+    <DatePicker
+      inline
+      selected={selectedDate}
+      onChange={date => setSelectedDate(date)}
+      dateFormat={"dd/mm/yyyy"}
+    />
+  </div>
+  
+  );
 }
 
 export default OnlineBooking;
