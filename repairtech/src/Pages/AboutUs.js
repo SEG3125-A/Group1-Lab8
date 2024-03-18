404574ea7dceb10fe3fb26a4fff98421fde628b3
Dante Deslauriers <64750637+ddesl069@users.noreply.github.com>
2024-03-17 23:20:39 -0400

    dd

--- repairtech/src/Pages/AboutUs.js
+++ repairtech/src/Pages/AboutUs.js
@@ -3,7 +3,6 @@ import React from 'react';
 function AboutUs() {
   return(
     <div className="about-us-container">
-      <h1 className="header">About Us</h1>
       <h2 className='header2'>Repair Any Tech, Anytime</h2>
       <p className="description">
         At RepairTech we are dedicated to helping you with all of your technology repair needs. Anytime we can't fix it, you get a guaranteed refund! We are a locally owned business for over 25 years of expertise! Check out our Services, Technicians and our Online Booking by clicking on the top right.
