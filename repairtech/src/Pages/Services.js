c7023c36c1c87c74020dff8e80c7ec0592e818ba
Dante Deslauriers <64750637+ddesl069@users.noreply.github.com>
2024-03-17 23:02:59 -0400

    dd

--- repairtech/src/Pages/Services.js
+++ repairtech/src/Pages/Services.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import { Card, Button, Row, Col } from 'react-bootstrap';
+
+const services = [
+  {
+    id: 1,
+    title: 'Apple products repair',
+    description: 'Look into the issue and repair to our best abilities. This includes, Iphones, Ipad, Macbooks',
+  },
+  {
+    id: 2,
+    title: 'Windows Laptops and PCs',
+    description: 'Repair and replace parts for your windows laptop or PC',
+  },
+  {
+    id: 3,
+    title: 'Android device',
+    description: 'High quality screen, battery, camera, etc... repairs. This is also included for Android tablets ',
+  },
+];
 
 function Services() {
-  return(
-    
-    
-    <div > Services</div>
-  
-  
-  
-  
+  return (
+    <div className='online-booking-container'>
+      <div className="container my-5">
+        <h2 className="text-center mb-4">Our Services</h2>
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {services.map((service) => (
+            <Col key={service.id}>
+              <Card className='service-card'>
+                <Card.Body>
+                  <Card.Title>{service.title}</Card.Title>
+                  <Card.Text>
+                    {service.description}
+                  </Card.Text>
+                  <Button className="service-btn" variant="primary">Learn More</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+     </div> 
   );
-
-
-  
 }
 
 export default Services;
